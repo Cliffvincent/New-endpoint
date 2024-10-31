5fa5f6e183efbd2fb7f72ea78fcc28d8dc69bd52
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-31 23:58:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,41 @@ app.get("/tempmail/message", async function (req, res) {
   res.sendFile(path.join(__dirname, "/public/disk/message"));
 });
 
+
+const UNSPLASH_ACCESS_KEY = 'RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw';
+
+app.get('/unsplash', async (req, res) => {
+  const search = req.query.search;
+  let count = parseInt(req.query.count) || 1;
+
+  if (!search) {
+    return res.status(400).json({ error: 'Search query is required' });
+  }
+
+  count = Math.min(count, 5);
+
+  const url = `https://api.unsplash.com/search/photos?query=${search}&per_page=${count}&client_id=${UNSPLASH_ACCESS_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    const cleanData = response.data.results.map((image) => ({
+      id: image.id,
+      description: image.description || 'No description available',
+      alt_description: image.alt_description,
+      urls: image.urls,
+      user: {
+        name: image.user.name,
+        portfolio_url: image.user.portfolio_url,
+      },
+    }));
+
+    res.json(cleanData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data from Unsplash' });
+  }
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
