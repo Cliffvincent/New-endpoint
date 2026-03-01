f0143e065ba5f2656fc02f6bc036c09cfb8f6828
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2026-03-01 13:25:37 +0000

    Bshs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const helmet = require("helmet");
 const express = require("express");
 const router = require("./api");
 const path = require("path");
@@ -7,8 +8,9 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
 app.get("/", async function (req, res) {
-  res.sendFile(path.join(__dirname, "/cliff/tik.html"));
+ res.sendFile(path.join(__dirname, "/cliff/tik.html"));
 });
 
 app.get("/ddos/site/spam", async function (req, res) {
@@ -169,7 +171,7 @@ app.get('/unsplash', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
